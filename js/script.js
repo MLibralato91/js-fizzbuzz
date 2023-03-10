3bff607a694423ffa43a4a5a243d22bcfbaab568
MLibralato91 <michelelibralato911@gmail.com>
2023-03-10 16:09:37 +0100

    change name variable

--- js/script.js
+++ js/script.js
@@ -9,22 +9,24 @@
 // 1. Scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
 // 2. Proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
 
+let treeAndFive = '';
 let tree = '';
+let five = '';
 
 for (let i = 0; i <= 100; i++) {
     const number = i;
     
     if (number % 3 === 0 && number % 5 === 0){
-        tree = number + "Il numero è divisibile per 3 e per 5"
-        console.log(tree);
+        treeAndFive = number + "Il numero è divisibile per 3 e per 5"
+        console.log(treeAndFive);
     }
     if (number % 3 === 0){
         tree = number + "Il numero è divisibile per 3"
         console.log(tree);
     }
     if (number % 5 === 0){
-        tree = number + "Il numero è divisibile per 5"
-        console.log(tree);
+        five = number + "Il numero è divisibile per 5"
+        console.log(five);
     }
 //aggiunto number per verificare da console che il risultato sia corretto
    
